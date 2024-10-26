9b9ea7d2ab97c95296848240ff51b5949e33d10b
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2024-10-26 13:04:32 +0100

    up

---
+++ src/app/simulation-types/page.tsx
@@ -0,0 +1,13 @@
+interface ISimulationType {}
+
+export default function SimulationType({}: ISimulationType) {
+  return (
+    <>
+      <h1>Simulation</h1>
+      <h1>Simulation</h1>
+      <h1>Simulation</h1>
+      <h1>Simulation</h1>
+      <h1>Simulation</h1>
+    </>
+  );
+}
