ca6e41e1c904fe23b02ce8c8b44454c6ac14b6f9
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2024-10-27 21:02:43 +0100

    mobile screen fixed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -9,18 +9,10 @@ export default function Auth() {
     <div className="min-h-screen bg-[url('/login.jpg')] bg-cover bg-center bg-no-repeat bg-fixed text-[#2c2c2c]">
       <div className=" w-screen h-screen backdrop-filter backdrop-blur-lg bg-opacity-30 flex items-center justify-center p-8">
         <div
-          className={` sm:flex items-center justify-between gap-x-6 shadow-xl rounded-lg p-8 bg-zinc-800 backdrop-filter backdrop-blur-lg border border-blue-300/30 bg-opacity-30`}
+          className={`  sm:flex items-center justify-between gap-x-6 sm:shadow-xl rounded-lg p-8 sm:bg-zinc-800 sm:backdrop-filter sm:backdrop-blur-lg sm:border sm:border-blue-300/30 sm:bg-opacity-30`}
         >
-          {/* <Image
-                className=""
-                src="/app-icons/wall1.jpg"
-                alt="Next.js logo"
-                width={680}
-                height={168}
-                priority
-              /> */}
           <div className="w-96  items-center grid ">
-            <h1 className="text-5xl font-bold text-center text-white">
+            <h1 className="text-5xl font-bold text-center text-white mb-4 sm:mb-0">
               Simulador de Seguro
             </h1>
           </div>
