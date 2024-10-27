7ab5e032fe4ad6b70ac980ce88c5e585336be59c
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2024-10-27 20:56:18 +0100

    login done

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,10 +8,10 @@ export default function Auth() {
   return (
     <div className="min-h-screen bg-[url('/login.jpg')] bg-cover bg-center bg-no-repeat bg-fixed text-[#2c2c2c]">
       <div className=" w-screen h-screen backdrop-filter backdrop-blur-lg bg-opacity-30 flex items-center justify-center p-8">
-          <div
-            className={` sm:flex items-center justify-between gap-x-6 shadow-xl rounded-lg p-8 bg-zinc-800 backdrop-filter backdrop-blur-lg border border-blue-300/30 bg-opacity-30`}
-          >
-            {/* <Image
+        <div
+          className={` sm:flex items-center justify-between gap-x-6 shadow-xl rounded-lg p-8 bg-zinc-800 backdrop-filter backdrop-blur-lg border border-blue-300/30 bg-opacity-30`}
+        >
+          {/* <Image
                 className=""
                 src="/app-icons/wall1.jpg"
                 alt="Next.js logo"
@@ -19,18 +19,31 @@ export default function Auth() {
                 height={168}
                 priority
               /> */}
-            <div className="w-96  items-center grid ">
-              <h1 className="text-5xl font-bold text-center text-white">
-                Simulador de Seguro
-              </h1>
-            </div>
-            <div className="bg-white/5 text-zinc-100 py-10 px-10 rounded-lg flex flex-col items-center justify-center gap-y-4">
-              <Input.Default icon={Mail} label="Email" />
-              <Input.Default icon={Lock} label="Senha" />
-              <button className="p-2 w-full bg-[#0f1b2d] rounded-lg">Entrar</button>
-              <Link href={""}>Recuperar Senha</Link>
-            </div>
+          <div className="w-96  items-center grid ">
+            <h1 className="text-5xl font-bold text-center text-white">
+              Simulador de Seguro
+            </h1>
           </div>
+          <div className="bg-white/5 text-zinc-100 py-10 px-10 rounded-lg flex flex-col items-center justify-center gap-y-4">
+            <Input.Default
+              icon={Mail}
+              iconColor="#fff"
+              borderColor="#fff"
+              label="Email"
+            />
+            <Input.Default
+              icon={Lock}
+              iconColor="#fff"
+              borderColor="#fff"
+              type="password"
+              label="Senha"
+            />
+            <button className="p-2 w-full bg-[#0f1b2d] rounded-lg">
+              Entrar
+            </button>
+            <Link href={""}>Recuperar Senha</Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/input/default.tsx
+++ src/components/input/default.tsx
@@ -2,15 +2,25 @@ import { ElementType } from "react";
 
 interface DefaultProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  icon: ElementType
+  icon: ElementType;
+  iconColor?: string;
+  borderColor?: string;
 }
-export function Default({ label,icon:Icon, ...rest }: DefaultProps) {
+export function Default({
+  label,
+  icon: Icon,
+  iconColor,
+  borderColor,
+  ...rest
+}: DefaultProps) {
   const randomUUID = crypto.randomUUID();
   return (
-    <div className=" space-y-2 bg">
-      <label htmlFor={randomUUID} className="font-bold">{label}</label>
-      <div className="flex items-center border gap-x-2 border-[#075985] rounded-xl p-3">
-        <Icon className="text-[#34475b]"/>
+    <div className=" space-y-2 ">
+      <label htmlFor={randomUUID} className="font-bold">
+        {label}
+      </label>
+      <div className={`${borderColor ? borderColor : "border-[#075985]"} flex items-center border gap-x-2 rounded-xl p-3`}>
+        <Icon className={iconColor ? iconColor : "text-[#34475b]"} />
         <input
           id={randomUUID}
           className="outline-none bg-transparent w-full"
