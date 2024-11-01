a7b585dc297aad91c8f45903321601abb82c82ac
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2024-11-01 17:36:33 +0100

    changing life ensurenre

--- src/app/simulation/particular/vida/page.tsx
+++ src/app/simulation/particular/vida/page.tsx
@@ -1,9 +1,30 @@
+import { Input } from "@/components/input";
+import { User2 } from "lucide-react";
+import Image from "next/image";
+
 interface IVida {}
 
-export default function Vida({}:IVida) {
-    return (
+export default function Vida({}: IVida) {
+  return (
+    <div className="text-zinc-800 bg-[#eff4f9] min-h-screen p-4 grid place-items-center">
+      <div className="bg-white p-4 rounded-lg min-h-[35rem]">
+        <Image
+          src="/app-icons/save-money.png"
+          alt="logo"
+          width={100}
+          height={100}
+        />
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, minus explicabo modi ea exercitationem mollitia assumenda illo expedita quas eligendi, sequi error iure suscipit repellat laboriosam placeat reiciendis maxime quasi!
+          <Input.Default icon={User2} label="Nome" />
+          <Input.Default icon={User2} label="Sobre Nome" />
+          <Input.Default icon={User2} label="Bilhete de Identidade" />
+          <Input.Default icon={User2} label="Gênero" />
+          <Input.Default icon={User2} label="Data de Nascimento" />
+          <hr />
+          <Input.Default icon={User2} label="Valor da Cobertura" />
+          <Input.Default icon={User2} label="Duração do Seguro em Ano" />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
