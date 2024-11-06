a32933fd0f080fd3e7a5587fb91ae429cde178df
Manuel Honoré de Sousa <sousa64manuel@gmail.com>
2024-11-06 14:27:58 +0100

    added all steps screen

--- src/app/simulation/particular/transporte/page.tsx
+++ src/app/simulation/particular/transporte/page.tsx
@@ -3,8 +3,9 @@
 import React, { useState } from "react";
 import { Button, message, Steps } from "antd";
 import { Input } from "@/components/input";
-import { FlagIcon, User2 } from "lucide-react";
+import { CalendarDays, Earth, FlagIcon, User2 } from "lucide-react";
 import { Check } from "@/components/check";
+import { title } from "process";
 
 
 // Informações do tomador de seguro e segurado
@@ -19,40 +20,26 @@ import { Check } from "@/components/check";
 export default function Transporte() {
   const steps = [
     {
-      title: "Passo 1",
+      title: "1º Passo",
       content: <StepOne title="Informações do tomador de seguro e segurado" />,
     },
     {
-      title: "Passo 2",
+      title: "2º Passo",
       content: (
         <StepTwo />
       ),
     },
     {
-      title: "Passo 3",
+      title: "3º Passo",
       content: <StepThree title="Distância e Destino" />,
     },
     {
-      title: "Passo 4",
-      content: <StepModel title="...." />,
+      title: "4º Passo",
+      content: <StepFour title="Detalhes adicionais & Condições Especiais" />,
     },
     {
-      title: "Passo 5",
-      content: <StepModel title="Detalhes adicionais" />,
-    },
-    {
-      title: "Passo 6",
-      content: <StepModel title="Condições Especiais" />,
-    },
-    {
-      title: "Passo 7",
-      content: (
-        <StepModel title="Condições de Manuseio e Embalagem da Mercadoria" />
-      ),
-    },
-    {
-      title: "Passo 8",
-      content: <StepModel title="Coberturas" />,
+      title: "5º Passo",
+      content: <StepFive title="" />,
     },
   ];
 
@@ -71,10 +58,10 @@ export default function Transporte() {
   return (
     <div className="text-gray-600 bg-[#eff4f9] lg:bg-[url('/blob-scene1.svg')] bg-cover bg-center bg-no-repeat bg-fixed min-h-screen p-4 grid place-items-center">
       {/* <div className="text-gray-600 bg-[#eff4f9] lg:bg-[url('/wavess.svg')] bg-cover bg-center bg-no-repeat bg-fixed min-h-screen p-4 grid place-items-center"> */}
-      <div className="bg-white  p-4 rounded-lg min-h-[35rem] shadow-lg flex flex-col justify-between">
+      <div className="bg-white w-[62rem] p-4 rounded-lg min-h-[35rem] shadow-lg flex flex-col justify-between">
         <div>
           <Steps current={current} items={items} />
-          <div className="h-[30rem] overflow-y-auto mt-4">
+          <div className="h-[30rem] w-full overflow-y-auto mt-4">
             {steps[current].content}
           </div>
         </div>
@@ -171,13 +158,13 @@ function StepTwo({  }: { title?: string }) {
           <Check.Radio
             itemList={Fake_Radio}
             defaultValue="1"
-            className="flex-col items-start *:w-full gap-y-2 *:text-start *:justify-start"
+            className="grid grid-cols-2 gap-2 items-start *:w-full gap-y-2 *:text-start *:justify-start"
           />
         </div>
         <div>
           <StepHeader title="Meio de Transporte" />
           <Check.CheckBox
-            className="gap-2 *:p-3"
+            className="gap-2 *:p-3 grid grid-cols-2"
             // activeBoxies={seguradoraSelectionadas}
             // setActiveBoxies={setSeguradoraSelectionadas}
             data={Fake_List}
@@ -203,29 +190,134 @@ function StepThree({  }: { title?: string }) {
   ];
 
   return (
-    <div className="  pt-4 px-2 ">
-      {/* <StepHeader title={""} /> */}
-      <div className="grid gap-6  ">
-        <div>
-          <StepHeader title="Classificação do Produto Transportado" />
+    <div className="  pt-4 px-2  h-full flex flex-col">
+      {/* <StepHeader title={title} /> */}
+      <div className="flex flex-col flex-1 *:flex-1 ">
+        <div className="">
+          <StepHeader title="País de Origem" />
           
-          <Input.Default icon={FlagIcon} title="Pais de Origem"/>
+          <Input.Default icon={Earth} title="Pais de Origem"/>
         </div>
-        <div>
-          <StepHeader title="Meio de Transporte" />
-         <Input.Default icon={FlagIcon} title="Pais de Destino"/>
+        <div className="">
+          <StepHeader title="País de Destino" />
+         <Input.Default icon={Earth} title="Pais de Destino"/>
         </div>
       </div>
     </div>
   );
 }
-
-function StepModel({ title }: { title: string }) {
+function StepFour({  }: { title?: string }) {
+  const Fake_List = [
+    { id: 0, name: "Nacional", value: "Nacional" },
+    { id: 1, name: "Interprovincial", value: "Interprovincial" },
+    { id: 2, name: "Países da SADC", value: "Países da SADC" },
+    { id: 3, name: "Sem transbordo", value: "Sem transbordo" },
+    { id: 4, name: "Intra-provincial", value: "Intra-provincial" },
+    { id: 5, name: "Outros Continentes", value: "Outros Continentes" },
+  ];
+  const Fake_List2 = [
+    { id: 0, name: "Cobertura para Avaria/Perda Total", value: "Cobertura para Avaria/Perda Total" },
+    { id: 1, name: "Roubo/Pirataria (marítimo)", value: "Roubo/Pirataria (marítimo)" },
+    { id: 2, name: "Cobertura para eventos climáticos severos", value: "Cobertura para eventos climáticos severos" },
+    { id: 3, name: "Riscos Geopolíticos (Áreas de conflito)", value: "Riscos Geopolíticos (Áreas de conflito)" },
+  ];
 
   return (
+    // Rascunho 
+    // <div className="  pt-4 px-2  h-full flex flex-col">
+    // <div className="flex flex-col flex-1 *:flex-1 ">
+
+
     <div className="  pt-4 px-2 ">
       {/* <StepHeader title={""} /> */}
-      Em desevolvimento... {title}
+      <div className="grid gap-6  ">
+      <div>
+          <StepHeader title="Detalhes Adicionais" />
+          <Check.CheckBox
+            className="gap-2 *:p-3 grid grid-cols-2"
+            // activeBoxies={seguradoraSelectionadas}
+            // setActiveBoxies={setSeguradoraSelectionadas}
+            data={Fake_List}
+          />
+        </div>
+      <div>
+          <StepHeader title="Condições Especiais" />
+          <Check.CheckBox
+            className="gap-2 *:p-3 grid grid-cols-2"
+            // activeBoxies={seguradoraSelectionadas}
+            // setActiveBoxies={setSeguradoraSelectionadas}
+            data={Fake_List2}
+          />
+        </div>
+
+
+        {/* <div>
+          <StepHeader title="Classificação do Produto Transportado" />
+          <Check.Radio
+            itemList={Fake_Radio}
+            defaultValue="1"
+            className="grid grid-cols-2 gap-2 items-start *:w-full gap-y-2 *:text-start *:justify-start"
+          />
+        </div> */}
+       
+      </div>
     </div>
   );
 }
+function StepFive({  }: { title?: string }) {
+  const Fake_Radio = [
+    { id: "1", value: "Embalados profissionalmente" },
+    { id: "2", value: "Sem proteção ou embalagem inadequada" },
+  ];
+  const Fake_Radio2 = [
+    { id: "1", value: "Cláusula A" },
+    { id: "2", value: "Cláusula B" },
+    { id: "3", value: "Cláusula C" },
+  ];
+
+ 
+
+  return (
+ 
+    <div className="  pt-4 px-2 ">
+      {/* <StepHeader title={""} /> */}
+      <div className="grid gap-6  ">
+     
+
+
+        <div>
+          <StepHeader title="Condições de Manuseio e Embalagem da Mercadoria" />
+          <Check.Radio
+            itemList={Fake_Radio}
+            defaultValue="1"
+            className="grid grid-cols-2 gap-2 items-start *:w-full gap-y-2 *:text-start *:justify-start"
+          />
+        </div>
+        <div >
+          <StepHeader title="Coberturas" />
+          <Check.Radio
+            itemList={Fake_Radio2}
+            defaultValue="1"
+            className="grid grid-cols-2 gap-2 items-start *:w-full gap-y-2 *:text-start *:justify-start"
+          />
+          <div className="space-y-2 mt-4">
+            <Input.Default label="Dias de duração da apolice" placeholder="0" icon={CalendarDays}/>
+            <Input.Default label="Valor máximo por mercadoria" placeholder="0" icon={CalendarDays}/>
+          </div>
+        </div>
+       
+       
+      </div>
+    </div>
+  );
+}
+
+// function StepModel({ title }: { title: string }) {
+
+//   return (
+//     <div className="  pt-4 px-2 ">
+//       {/* <StepHeader title={""} /> */}
+//       Em desevolvimento... {title}
+//     </div>
+//   );
+// }
