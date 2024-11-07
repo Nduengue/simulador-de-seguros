cc70047f489ad4309db49e24b4b9cddb74c50515
Lopes Raimundo Cristovao <tiloypedro@gmail.com>
2024-11-07 15:17:39 +0100

    versao_1

--- app/Http/Controllers/rotaApiControllers.php
+++ app/Http/Controllers/rotaApiControllers.php
@@ -2,20 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-/* class rotaApiControllers extends Controller
-{
-    public static function getUrl()
-    {
-        // local
-        $api_url = 'https://api-simulator.mtapp.ao/';
-        // servidor
-        // $api_url = 'http://0.0.0.0:8008/';
-        return $api_url;
-    }
-}
- */
 
  abstract class rotaApiControllers{
     protected static $api_url = 'https://api-simulator.mtapp.ao/';
@@ -24,4 +10,3 @@ use Illuminate\Http\Request;
         return self::$api_url;
     }
 }       
- 
