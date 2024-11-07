6fd46f13c783f6fbd8aa31c1af11a62d2f1af3d0
Lopes Raimundo Cristovao <tiloypedro@gmail.com>
2024-11-07 13:51:26 +0100

    Meu projecto 13/43:07/11/2024

--- app/Http/Controllers/api.php
+++ app/Http/Controllers/api.php
@@ -4,9 +4,9 @@ class urlApi
     public static function getUrl()
     {
         // local
-        // $api_url = 'http://api-simulator.mtapp.ao/';
+        $api_url = 'http://api-simulator.mtapp.ao/';
         // servidor
-        $api_url = 'http://0.0.0.0:8008/';
+        // $api_url = 'http://0.0.0.0:8008/';
         return $api_url;
     }
 }
